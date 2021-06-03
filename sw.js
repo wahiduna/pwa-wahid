f9465995c148a2fc7b16d8986eb96739929bdf9c
Wahid Romdhoni <wahidromdhs@gmail.com>
2021-06-03 11:20:42 +0700

    update service worker

--- sw.js
+++ sw.js
@@ -1,38 +1,38 @@
-const cacheName = "mgs-cache-v1";
+const cacheName = 'mgs-cache-v1';
 const assets = [
-    "/",
-    "index.html",
-    "css/style.css",
-    "js/app.js",
-    "assets/img/favicon.png",
-    "assets/img/apple-touch-icon.png",
-    "assets/vendor/aos/aos.css",
-    "assets/vendor/bootstrap/css/bootstrap.min.css",
-    "assets/vendor/bootstrap-icons/bootstrap-icons.css",
-    "assets/vendor/boxicons/css/boxicons.min.css",
-    "assets/vendor/glightbox/css/glightbox.min.css",
-    "assets/vendor/swiper/swiper-bundle.min.css",
-    "assets/css/style.css",
-    "assets/vendor/aos/aos.js",
-    "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
-    "assets/vendor/glightbox/js/glightbox.min.js",
-    "assets/vendor/isotope-layout/isotope.pkgd.min.js",
-    "assets/vendor/php-email-form/validate.js",
-    "assets/vendor/swiper/swiper-bundle.min.js"
+    '/',
+    'index.html',
+    'css/style.css',
+    'js/app.js',
+    'assets/img/favicon.png',
+    'assets/img/apple-touch-icon.png',
+    'assets/vendor/aos/aos.css',
+    'assets/vendor/bootstrap/css/bootstrap.min.css',
+    'assets/vendor/bootstrap-icons/bootstrap-icons.css',
+    'assets/vendor/boxicons/css/boxicons.min.css',
+    'assets/vendor/glightbox/css/glightbox.min.css',
+    'assets/vendor/swiper/swiper-bundle.min.css',
+    'assets/css/style.css',
+    'assets/vendor/aos/aos.js',
+    'assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+    'assets/vendor/glightbox/js/glightbox.min.js',
+    'assets/vendor/isotope-layout/isotope.pkgd.min.js',
+    'assets/vendor/php-email-form/validate.js',
+    'assets/vendor/swiper/swiper-bundle.min.js'
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
+self.addEventListener("install", event => {
+    event.waitUntil(
     caches.open(cacheName).then(cache => {
       cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request);
+self.addEventListener('fetch', event => {
+    event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
